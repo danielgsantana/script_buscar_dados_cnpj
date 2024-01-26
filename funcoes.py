98b7beb67419305789aa6a2012c6831b3e3f93d8
danielgsantana <danielgustsantana@gmail.com>
2024-01-26 07:29:00 -0300

    Adicionado o retorno das informações para a planilha, limpeza de codigo

--- funcoes.py
+++ funcoes.py
@@ -1,10 +1,17 @@
 from openpyxl import Workbook, load_workbook
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import requests
+import time
 import json
+import re
+
 import Empresa
 
 #percorrendo a planilha para adicionar na lista
-def buscar_cnpj (list):
+def capturar_cnpj (list) :
     
     empresasList = []
     for celula in list["A"]:
@@ -15,9 +22,47 @@ def buscar_cnpj (list):
 
     return empresasList
 
+def buscar_cnpj (empresasList) :
+
+    # Inicia o uma instância do google webdriver
+    service = Service()
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+
+    # Acessando a URL
+    url = 'https://cnpj.linkana.com/'
+
+    # Pesquisando os nomes das empresas da planilha e adicionando em uma lista
+    cnpj_list= []
+
+    driver.get(url)
+    time.sleep(0.5)
+    driver.find_element(By.XPATH, '//*[@id="q"]').click()
+    driver.find_element(By.XPATH, '//*[@id="q"]').send_keys('azship')
+    driver.find_element(By.XPATH, '/html/body/div[1]/main/div[1]/div/div[2]/form/div/input[2]').click()
+    time.sleep(0.5)
+
+    for name in empresasList:
+        
+        driver.find_element(By.XPATH, '//*[@id="q"]').click()
+        driver.find_element(By.XPATH, '//*[@id="q"]').send_keys(name)
+        driver.find_element(By.XPATH, '/html/body/div/main/div/form/div[2]/input').click()
+        time.sleep(0.5)
+        
+        try:
+            c = driver.find_element(By.XPATH, '/html/body/div/main/div/div/a/div/div/p[2]').text
+            cnpj = re.sub(r'[^0-9]', '', c)
+            cnpj_list.append(cnpj)
+
+        except NoSuchElementException:
+            c = f"{name}"     # parei aqui, não posso adicionar uma string em uma lista de inteiro
+            cnpj_list.append(c)
+        
+
+    return cnpj_list
 
 # Loop sobre os CNPJs
-def buscar_cnpj_api(cnpj_list):
+def buscar_cnpj_api(cnpj_list) :
     
     # Substitua "seu_token_aqui" pelo seu token real
     token = "f2987ff033bd4550a7a208f4fc82be13c3ea145f2369561b089cba941742eda6"
@@ -65,18 +110,42 @@ def buscar_cnpj_api(cnpj_list):
 
     return response_cnpj
 
-def tratar_dados_api (objetos_api):
-    objeto_tratado = {
-        "cnpj": None,
-        "Razão Social": None,
-        "Nome Fantasia": None,
-        "Situação Cadastral": None,
-        "Data de Abertura": None,
-        "Capital Social": None,
-        "Email": None,
-        "Telefone": None,
-        "Município": None,
-        "UF": None,
-        "CEP": None,
-        "CNAE Principal": None,
-    }
+def adicionar_dados_planilha (empresasList, empresas_result) :
+    
+    planilha = Workbook()
+    aba_ativa = planilha.active
+
+    aba_ativa['A1'] = 'NOME EMPRESA'
+    aba_ativa['B1'] = 'CNPJ'
+    aba_ativa['C1'] = 'RAZÃO SOCIAL'
+    aba_ativa['D1'] = 'NOME FANTASIA'
+    aba_ativa['E1'] = 'ABERTURA'
+    aba_ativa['F1'] = 'CAPITAL'
+    aba_ativa['G1'] = 'EMAIL'
+    aba_ativa['H1'] = 'TELEFONE'
+    aba_ativa['I1'] = 'MUNICIPIO'
+    aba_ativa['J1'] = 'UF'
+    aba_ativa['K1'] = 'CEP'
+    aba_ativa['L1'] = 'CNAE PRINCIPAL'
+
+    row = 2
+    repetidor = 0
+
+    for rows in empresas_result :
+        aba_ativa[f'A{row}'] = empresasList[repetidor]
+        aba_ativa[f'B{row}'] = empresas_result[repetidor].cnpj
+        aba_ativa[f'C{row}'] = empresas_result[repetidor].razao_social
+        aba_ativa[f'D{row}'] = empresas_result[repetidor].nome_fantasia
+        aba_ativa[f'E{row}'] = empresas_result[repetidor].abertura
+        aba_ativa[f'F{row}'] = empresas_result[repetidor].capital
+        aba_ativa[f'G{row}'] = empresas_result[repetidor].email
+        aba_ativa[f'H{row}'] = empresas_result[repetidor].telefone
+        aba_ativa[f'I{row}'] = empresas_result[repetidor].municipio
+        aba_ativa[f'J{row}'] = empresas_result[repetidor].uf
+        aba_ativa[f'K{row}'] = empresas_result[repetidor].cep
+        aba_ativa[f'L{row}'] = empresas_result[repetidor].cnae
+
+        repetidor += 1 
+        row += 1
+
+    planilha.save('Dados_Gerados.xlsx')
--- main.py
+++ main.py
@@ -7,47 +7,22 @@ import re
 import time
 
 
-#importando a planilha
+#importando a planilha (INSERIR O NOME DO ARQUIVO EXCEL QUE CONTENHA OS NOMES DAS EMPRESAS)
 planilha = load_workbook("teste.xlsx")
-
-#definindo a aba ativa
+ 
 aba_ativa = planilha.active
-#pagina = planilha['deals list']
-pagina = planilha['companies']
-    
 
 # Percorrendo a planilha para adicionar na lista
-empresasList = funcoes.buscar_cnpj(aba_ativa)
+empresasList = funcoes.capturar_cnpj(aba_ativa)
 
-# Inicia o uma instância do google webdriver
-service = Service()
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
 
-# Acessando a URL
-url = 'https://cnpj.linkana.com/'
-
-# Pesquisando os nomes das empresas da planilha e adicionando em uma lista
-cnpj_list= []
-for name in empresasList:
-    driver.get(url)
-    time.sleep(0.5)
-    driver.find_element(By.XPATH, '//*[@id="q"]').click()
-    driver.find_element(By.XPATH, '//*[@id="q"]').send_keys(name)
-    driver.find_element(By.XPATH, '/html/body/div[1]/main/div[1]/div/div[2]/form/div/input[2]').click()
-    time.sleep(0.5)
-    c = driver.find_element(By.XPATH, '/html/body/div/main/div/div/a/div/div/p[2]').text
-
-    cnpj = re.sub(r'[^0-9]', '', c)
-
-    cnpj_list.append(cnpj) #talvez substituir o nome da empresa na 'empresaList' pelo cnpj
-
+cnpj_list = funcoes.buscar_cnpj(empresasList)
 
 
 empresas_result = funcoes.buscar_cnpj_api(cnpj_list)
 
 
-
+funcoes.adicionar_dados_planilha(empresasList, empresas_result)
 
 
 print('ola mundo')
