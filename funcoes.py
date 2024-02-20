d3f5b39bda4c9d400a629588b31ee793ee8ada17
Daniel Gustavo - Trabalho <daniel.azship@gmail.com>
2024-02-20 16:53:40 -0300

    Corrigindo limitação do Linkana e registrando dados obtidos apos realizar a pesquisa

--- funcoes.py
+++ funcoes.py
@@ -15,9 +15,13 @@ def capturar_cnpj (list) :
     
     empresasList = []
     for celula in list["A"]:
-
-        # Removendo caracteres especiais usando expressões regulares
-        d = re.sub(r'[^A-Za-z0-9 ]+', '', celula.value)
+        
+        if celula.value == None:
+            break
+            
+        else:
+            # Removendo caracteres especiais usando expressões regulares
+            d = re.sub(r'[^A-Za-z0-9 ]+', '', celula.value)
 
         # Substituindo dois espaços consecutivos por apenas um espaço
         empresa = re.sub(r'  +', ' ', d)
@@ -67,7 +71,7 @@ def buscar_cnpj (empresasList) :
     return cnpj_list
 
 # Loop sobre os CNPJs
-def buscar_cnpj_api(cnpj_list) :
+def buscar_cnpj_api(cnpj) :
     
     # Substitua "seu_token_aqui" pelo seu token real
     token = "f2987ff033bd4550a7a208f4fc82be13c3ea145f2369561b089cba941742eda6"
@@ -78,93 +82,63 @@ def buscar_cnpj_api(cnpj_list) :
         "Content-Type": "application/json"
     }
 
-    response_cnpj = []
-    for cnpj in cnpj_list:
+    if cnpj == "CNPJ não encontrado":
+        c = Empresa.Empresa(cnpj='erro',
+                            razao_social= 'erro',
+                            nome_fantasia= 'erro',
+                            abertura= 'erro',
+                            capital= 'erro',
+                            email= 'erro',
+                            telefone= 'erro',
+                            municipio= 'erro',
+                            uf= 'erro',
+                            cep= 'erro',
+                            cnae = 'erro')
         
-        if cnpj == "CNPJ não encontrado":
-            c = Empresa.Empresa(cnpj='erro',
-                                razao_social= 'erro',
-                                nome_fantasia= 'erro',
-                                abertura= 'erro',
-                                capital= 'erro',
-                                email= 'erro',
-                                telefone= 'erro',
-                                municipio= 'erro',
-                                uf= 'erro',
-                                cep= 'erro',
-                                cnae = 'erro')
-            
-            response_cnpj.append(c)
-            
-        else:
-            # Construa a URL com a variável 'cnpj'
-            url = f"https://receitaws.com.br/v1/cnpj/{cnpj}/days/5"
+        return c
+    else:
+       # Construa a URL com a variável 'cnpj'
+       url = f"https://receitaws.com.br/v1/cnpj/{cnpj}/days/5"
 
-            # Fazer a requisição GET
-            response = requests.get(url, headers=headers)
+       # Fazer a requisição GET
+       response = requests.get(url, headers=headers)
 
+       # Verificar se a requisição foi bem-sucedida (código 200)
+       if response.status_code == 200: 
+           
+           # Processar os dados da resposta
+           response = response.json()
 
-            # Verificar se a requisição foi bem-sucedida (código 200)
-            if response.status_code == 200:
-                # Processar os dados da resposta
-                response = response.json()
+           c = Empresa.Empresa(cnpj=response['cnpj'],
+                           razao_social=response['nome'],
+                           nome_fantasia=response['fantasia'],
+                           abertura=response['abertura'],
+                           capital=response['capital_social'],
+                           email=response['email'],
+                           telefone=response['telefone'],
+                           municipio=response['municipio'],
+                           uf=response['uf'],
+                           cep=response['cep'],
+                           cnae = response.get('atividade_principal', [{}])[0].get('text', ''))
+           
+           return c
 
-                objeto = Empresa.Empresa(cnpj=response['cnpj'],
-                                razao_social=response['nome'],
-                                nome_fantasia=response['fantasia'],
-                                abertura=response['abertura'],
-                                capital=response['capital_social'],
-                                email=response['email'],
-                                telefone=response['telefone'],
-                                municipio=response['municipio'],
-                                uf=response['uf'],
-                                cep=response['cep'],
-                                cnae = response.get('atividade_principal', [{}])[0].get('text', ''))
-
-                response_cnpj.append(objeto)
-
-            else:
-                # Lidar com erros
-                print(f"Erro para CNPJ {cnpj}: {response.status_code} - {response.text}")
-
-    return response_cnpj
-
-def adicionar_dados_planilha (empresasList, empresas_result) :
+def adicionar_dados_planilha (nome_empresa, empresas_result, planilha, row) :
     
-    planilha = Workbook()
-    aba_ativa = planilha.active
+    
+    aba_ativa = planilha['resultado gerado']
+    
+    aba_ativa[f'A{row}'] = nome_empresa
+    aba_ativa[f'B{row}'] = empresas_result.cnpj
+    aba_ativa[f'C{row}'] = empresas_result.razao_social
+    aba_ativa[f'D{row}'] = empresas_result.nome_fantasia
+    aba_ativa[f'E{row}'] = empresas_result.abertura
+    aba_ativa[f'F{row}'] = empresas_result.capital
+    aba_ativa[f'G{row}'] = empresas_result.email
+    aba_ativa[f'H{row}'] = empresas_result.telefone
+    aba_ativa[f'I{row}'] = empresas_result.municipio
+    aba_ativa[f'J{row}'] = empresas_result.uf
+    aba_ativa[f'K{row}'] = empresas_result.cep
+    aba_ativa[f'L{row}'] = empresas_result.cnae
 
-    aba_ativa['A1'] = 'NOME EMPRESA'
-    aba_ativa['B1'] = 'CNPJ'
-    aba_ativa['C1'] = 'RAZÃO SOCIAL'
-    aba_ativa['D1'] = 'NOME FANTASIA'
-    aba_ativa['E1'] = 'ABERTURA'
-    aba_ativa['F1'] = 'CAPITAL'
-    aba_ativa['G1'] = 'EMAIL'
-    aba_ativa['H1'] = 'TELEFONE'
-    aba_ativa['I1'] = 'MUNICIPIO'
-    aba_ativa['J1'] = 'UF'
-    aba_ativa['K1'] = 'CEP'
-    aba_ativa['L1'] = 'CNAE PRINCIPAL'
-
-    row = 2
-    repetidor = 0
-
-    for rows in empresas_result :
-        aba_ativa[f'A{row}'] = empresasList[repetidor]
-        aba_ativa[f'B{row}'] = empresas_result[repetidor].cnpj
-        aba_ativa[f'C{row}'] = empresas_result[repetidor].razao_social
-        aba_ativa[f'D{row}'] = empresas_result[repetidor].nome_fantasia
-        aba_ativa[f'E{row}'] = empresas_result[repetidor].abertura
-        aba_ativa[f'F{row}'] = empresas_result[repetidor].capital
-        aba_ativa[f'G{row}'] = empresas_result[repetidor].email
-        aba_ativa[f'H{row}'] = empresas_result[repetidor].telefone
-        aba_ativa[f'I{row}'] = empresas_result[repetidor].municipio
-        aba_ativa[f'J{row}'] = empresas_result[repetidor].uf
-        aba_ativa[f'K{row}'] = empresas_result[repetidor].cep
-        aba_ativa[f'L{row}'] = empresas_result[repetidor].cnae
-
-        repetidor += 1 
-        row += 1
-
-    planilha.save('Dados_Gerados.xlsx')
+    planilha.save('planilha inicial.xlsx')
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from openpyxl import Workbook, load_workbook
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import funcoes
 import re
 import time
@@ -33,29 +34,77 @@ while True:
 if entrada == 1:
 
     #importando a planilha (INSERIR O NOME DO ARQUIVO EXCEL QUE CONTENHA OS NOMES DAS EMPRESAS)
-    planilha = load_workbook("nome empresas.xlsx")
+    planilha = load_workbook("planilha inicial.xlsx")
     
     aba_ativa = planilha['principal']
 
     # Percorrendo a planilha para adicionar na lista
-    empresasList = funcoes.capturar_cnpj(aba_ativa)
+    empresa_nome_list = funcoes.capturar_cnpj(aba_ativa)
 
-    cnpj_list = funcoes.buscar_cnpj(empresasList)
+    # Inicia o uma instância do google webdriver
+    service = Service()
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
 
-    empresas_result = funcoes.buscar_cnpj_api(cnpj_list)
+    # Acessando a URL
+    url = 'https://cnpj.linkana.com/'
 
-    funcoes.adicionar_dados_planilha(empresasList, empresas_result)
+    driver.get(url)
+
+    time.sleep(1)
+    driver.find_element(By.XPATH, '//*[@id="q"]').click()
+    driver.find_element(By.XPATH, '//*[@id="q"]').send_keys('azship')
+    driver.find_element(By.XPATH, '/html/body/div[1]/main/div[1]/div/div[2]/form/div/input[2]').click()
+
+    aba_ativa = planilha.create_sheet(title='resultado gerado')
+
+    aba_ativa['A1'] = 'NOME EMPRESA'
+    aba_ativa['B1'] = 'CNPJ'
+    aba_ativa['C1'] = 'RAZÃO SOCIAL'
+    aba_ativa['D1'] = 'NOME FANTASIA'
+    aba_ativa['E1'] = 'ABERTURA'
+    aba_ativa['F1'] = 'CAPITAL'
+    aba_ativa['G1'] = 'EMAIL'
+    aba_ativa['H1'] = 'TELEFONE'
+    aba_ativa['I1'] = 'MUNICIPIO'
+    aba_ativa['J1'] = 'UF'
+    aba_ativa['K1'] = 'CEP'
+    aba_ativa['L1'] = 'CNAE PRINCIPAL'
+
+    row = 2
+
+    for item in empresa_nome_list:
+
+        time.sleep(3)
+        driver.find_element(By.XPATH, '//*[@id="q"]').click()
+        driver.find_element(By.XPATH, '//*[@id="q"]').send_keys(item)
+        driver.find_element(By.XPATH, '/html/body/div/main/div/form/div[2]/input').click()
+
+        try:
+            c = driver.find_element(By.XPATH, '/html/body/div/main/div/div/a/div/div/p[2]').text
+            cnpj = re.sub(r'[^0-9]', '', c)
+
+        except NoSuchElementException:
+            cnpj = "CNPJ não encontrado"
+
+        objeto_cnpj = funcoes.buscar_cnpj_api(cnpj)
+
+
+        funcoes.adicionar_dados_planilha(item, objeto_cnpj, planilha, row)
+
+        row += 1
+
 
 if entrada == 2:
 
     #importando a planilha (INSERIR O NOME DO ARQUIVO EXCEL QUE CONTENHA OS NOMES DAS EMPRESAS)
-    planilha = load_workbook("cnpj empresas.xlsx")
+    planilha = load_workbook("planilha inicial.xlsx")
     
     aba_ativa = planilha['principal']
 
     # Percorrendo a planilha para adicionar na lista
-    empresasList = funcoes.capturar_cnpj(aba_ativa)
+    cnpj_list = funcoes.capturar_cnpj(aba_ativa)
 
-    empresas_result = funcoes.buscar_cnpj_api(empresasList)
+    empresas_result = funcoes.buscar_cnpj_api(cnpj_list)
 
-    funcoes.adicionar_dados_planilha(empresasList, empresas_result)
+    funcoes.adicionar_dados_planilha(cnpj_list, empresas_result)
